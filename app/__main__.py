f6861f9e267158f08483ecdb3fec2772a7bac11a
v-dvorak <vojtech.dvorak11@seznam.cz>
2024-11-01 21:54:19 +0100

    Move validation inside source

--- app/__main__.py
+++ app/__main__.py
@@ -11,31 +11,31 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="Object Detection Tools"
     )
-
-    parser.add_argument("--config", default=None,
-                        help="Path to config, see \"default_config.json\" for example.")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Make script verbose")
-
     subparsers = parser.add_subparsers(dest="command", help="Jobs")
 
     # DATASET FORMATTING AND SPLITTING
-    proc_parser = subparsers.add_parser("proc")
-    proc_parser.add_argument("output", help="Transformed dataset destination.")
-    proc_parser.add_argument("images_path", help="Path to images.")
-    proc_parser.add_argument("annot_path", help="Path to subpages.")
+    form_parser = subparsers.add_parser("form")
+    form_parser.add_argument("output", help="Transformed dataset destination.")
+    form_parser.add_argument("images_path", help="Path to images.")
+    form_parser.add_argument("annot_path", help="Path to subpages.")
 
-    proc_parser.add_argument("-i", "--input_format", default="mung", choices=["mung", "coco", "yolod", "yolos"])
-    proc_parser.add_argument("-o", "--output_format", default="coco", choices=["mung", "coco", "yolod", "yolos"])
-    proc_parser.add_argument("--image_format", default="jpg", help="Input image format.")
+    form_parser.add_argument("-i", "--input_format", default="mung", choices=["mung", "coco", "yolod", "yolos"])
+    form_parser.add_argument("-o", "--output_format", default="coco", choices=["mung", "coco", "yolod", "yolos"])
+    form_parser.add_argument("--image_format", default="jpg", help="Input image format.")
 
-    proc_parser.add_argument("-s", "--split", type=float, default=1.0, help="Train/test split ratio.")
+    form_parser.add_argument("-s", "--split", type=float, default=1.0, help="Train/test split ratio.")
 
-    proc_parser.add_argument("--seed", type=int, default=42, help="Seed for dataset shuffling.")
-    proc_parser.add_argument("--config", default=None,
+    form_parser.add_argument("--seed", type=int, default=42, help="Seed for dataset shuffling.")
+    form_parser.add_argument("--config", default=None,
                              help="Path to config, see \"default_config.json\" for example.")
-    proc_parser.add_argument("--resize", type=int, default=None,
+    form_parser.add_argument("--resize", type=int, default=None,
                              help="Resizes images so that the longer side is this many pixels long.")
-    proc_parser.add_argument("--image_splitting", action="store_true", help="Split images into smaller ones.")
+    form_parser.add_argument("--image_splitting", action="store_true", help="Split images into smaller ones.")
+
+    # global arguments
+    form_parser.add_argument("-v", "--verbose", action="store_true", help="Make script verbose")
+    form_parser.add_argument("--config", default=None,
+                             help="Path to config, see \"default_config.json\" for example.")
 
     # DATASET STATISTICS
     stats_parser = subparsers.add_parser("stats")
@@ -44,6 +44,11 @@ if __name__ == "__main__":
 
     stats_parser.add_argument("-o", "--output", type=str, default=None, help="If used, plots will be saved here.")
 
+    # global arguments
+    stats_parser.add_argument("-v", "--verbose", action="store_true", help="Make script verbose")
+    stats_parser.add_argument("--config", default=None,
+                              help="Path to config, see \"default_config.json\" for example.")
+
     # MODEL VALIDATION
     val_parser = subparsers.add_parser("val")
 
@@ -51,13 +56,21 @@ if __name__ == "__main__":
     val_parser.add_argument("images_path", help="Path to images.")
     val_parser.add_argument("annot_path", help="Path to subpages.")
 
+    val_parser.add_argument("-i", "--input_format", default="yolod", choices=["mung", "coco", "yolod", "yolos"],
+                            help="Validation dataset annotation format.")
+    val_parser.add_argument("-m", "--model_type", default="yolod", choices=["yolod", "yolos"],
+                            help="Type of model.")
+
+    val_parser.add_argument("--image_format", default="jpg", help="Input image format.")
+
     val_parser.add_argument("-o", "--overlap", type=int, help="Overlap ratio for image splits.")
-    val_parser.add_argument("--config", default=None,
-                        help="Path to config, see \"default_config.json\" for example.")
     val_parser.add_argument("-c", "--count", type=int, help="How many images the model will be tested on.")
     val_parser.add_argument("-s", "--seed", type=int, default=42, help="Seed for dataset shuffling.")
-    val_parser.add_argument("-v", "--verbose", action="store_true", help="Make script verbose")
 
+    # global arguments
+    val_parser.add_argument("-v", "--verbose", action="store_true", help="Make script verbose")
+    val_parser.add_argument("--config", default=None,
+                            help="Path to config, see \"default_config.json\" for example.")
 
     args = parser.parse_args()
 
@@ -70,7 +83,7 @@ if __name__ == "__main__":
         with open(args.config, "r", encoding="utf8") as f:
             loaded_config = json.load(f)
 
-    if args.command == "proc":
+    if args.command == "form":
         input_f = Formatter.InputFormat.from_string(args.input_format)
         output_f = Formatter.OutputFormat.from_string(args.output_format)
 
@@ -115,8 +128,9 @@ if __name__ == "__main__":
         )
 
     elif args.command == "val":
-        from val import EvalJob, FScores
-        from val.EvalJob import ValBoundingBox
+        from .Val import EvalJob, FScores
+        from .Val.EvalJob import ValBoundingBox
+
         CLASSES = loaded_config["class_output_names"]
 
         GROUND_TRUTH, PREDICTIONS = EvalJob.yolo_val(
